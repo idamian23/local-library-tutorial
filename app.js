e9e4ed7fd2034b93b33615b3447cad3bff1ac32d
idamian23 <damian.ionut23@yahoo.ro>
2023-09-19 12:47:17 +0300

    Routes and Controllers

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ var logger = require("morgan");
 
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
+const catalogRouter = require("./routes/catalog");
 
 var app = express();
 
@@ -23,6 +24,7 @@ app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
 app.use("/users", usersRouter);
+app.use("/catalog", catalogRouter);
 
 const mongoDB = "mongodb://localhost:27017";
 
